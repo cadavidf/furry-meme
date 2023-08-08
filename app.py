4531588a8d6c580770e6727fc967af2e32b231a3
Felipe Cadavid <cadavidf@users.noreply.github.com>
2023-08-08 15:49:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -78,13 +78,13 @@ st.plotly_chart(bar_chart)
 
 
 #col1.dataframe(df[mask])
-
+colors_pie = ['#499bbd', '#60a7c5', '#76b4cd', '#8dc0d6', '#f7b176', '#f79e50', '#f7882c', '#f7720a']
 # --- PLOT PIE CHART
 pie_chart = px.pie(df_participants,
                 title='Total No. of Participants',
                 values='Participants',
                 color='Departments',
-                color_discrete_sequence= colors,
+                color_discrete_sequence= colors_pie,
                 color_discrete_map= group_color,
                 names='Departments')
 
